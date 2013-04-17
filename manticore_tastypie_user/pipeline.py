dff83f7061d80be28b06d74d8af6341ddd4dd6f8
Rudy <rudy@yetihq.com>
2013-04-17 14:46:19 -0700

    pull fb photo from social sign up

--- manticore_tastypie_user/pipeline.py
+++ manticore_tastypie_user/pipeline.py
@@ -1,7 +1,12 @@
+import urllib
+from urllib2 import URLError
+from django.core.files import File
 from manticore_tastypie_user.manticore_tastypie_user.resources import UserProfile
 
-def create_user_profile(backend, details, response, user=None, is_new=False, *args, **kwargs):
+
+def create_user_profile(backend, details, response, uid, user=None, is_new=False, *args, **kwargs):
     """Create a UserProfile for the User"""
+
     if user is None:
         return
 
@@ -9,4 +14,14 @@ def create_user_profile(backend, details, response, user=None, is_new=False, *ar
         user.get_profile()
     except UserProfile.DoesNotExist:
         user_profile = UserProfile(user=user)
+
+        # Save photo from FB
+        if backend.name == "facebook":
+            try:
+                image_url = "https://graph.facebook.com/%s/picture?type=large" % uid
+                result = urllib.urlretrieve(image_url)
+                user_profile.original_photo.save("%s.jpg" % uid, File(open(result[0])))
+            except URLError:
+                pass
+
         user_profile.save()
