c8420097f757452d81ee05d0d1aeeee0aa406ef6
Rudy <rudy@yetihq.com>
2013-05-28 15:31:19 -0700

    allow associating social user accounts with a different account if accessed

--- manticore_tastypie_user/pipeline.py
+++ manticore_tastypie_user/pipeline.py
@@ -1,12 +1,31 @@
-import cgi
 import json
 import urllib
 from urllib2 import URLError
-import urlparse
 from django.conf import settings
 from django.core.files import File
 import oauth2
+from social_auth.db.django_models import UserSocialAuth
 from manticore_tastypie_user.manticore_tastypie_user.resources import UserProfile
+
+
+def social_auth_user(backend, uid, user=None, *args, **kwargs):
+    """Return UserSocialAuth account for backend/uid pair or None if it
+    doesn't exists.
+
+    Delete UserSocialAuth if UserSocialAuth entry belongs to another
+    user.
+    """
+    social_user = UserSocialAuth.get_social_auth(backend.name, uid)
+    if social_user:
+        if user and social_user.user != user:
+            # Delete UserSocialAuth pairing so this account can now connect
+            social_user.delete()
+            social_user = None
+        elif not user:
+            user = social_user.user
+    return {'social_user': social_user,
+            'user': user,
+            'new_association': False}
 
 
 def create_user_profile(backend, details, response, uid, user=None, social_user=None, *args, **kwargs):
