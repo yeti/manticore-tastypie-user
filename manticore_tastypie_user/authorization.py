cfe1aee4b39d3fa980db8c45c582bca20407f156
Rudy Mutter <rudy@yetihq.com>
2014-03-10 14:17:28 -0700

    remove duplicated user authorization class

--- manticore_tastypie_user/authorization.py
+++ manticore_tastypie_user/authorization.py
@@ -36,41 +36,4 @@ class UserObjectsOnlyAuthorization(Authorization):
         raise Unauthorized("Sorry, no deletes.")
 
     def delete_detail(self, object_list, bundle):
-        raise Unauthorized("Sorry, no deletes.")
-
-class UserObjectsOnlyAuthorization(Authorization):
-    def read_list(self, object_list, bundle):
-        # This assumes a ``QuerySet`` from ``ModelResource``.
-        return object_list.filter(user=bundle.request.user)
-
-    def read_detail(self, object_list, bundle):
-        # Is the requested object owned by the user?
-        return bundle.obj.user == bundle.request.user
-
-    def create_list(self, object_list, bundle):
-        # Assuming their auto-assigned to ``user``.
-        return object_list
-
-    def create_detail(self, object_list, bundle):
-        bundle.obj.user = bundle.request.user
-        return True
-
-    def update_list(self, object_list, bundle):
-        allowed = []
-
-        # Since they may not all be saved, iterate over them.
-        for obj in object_list:
-            if obj.user == bundle.request.user:
-                allowed.append(obj)
-
-        return allowed
-
-    def update_detail(self, object_list, bundle):
-        return bundle.obj.user == bundle.request.user
-
-    def delete_list(self, object_list, bundle):
-        # Sorry user, no deletes for you!
-        raise Unauthorized("Sorry, no bulk deletes.")
-
-    def delete_detail(self, object_list, bundle):
         return bundle.obj.user == bundle.request.user
