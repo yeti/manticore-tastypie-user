663a99996d830b8d9a8f3be7e03bbf0014a67409
Rudy Mutter <rudy@yetihq.com>
2014-03-10 20:22:51 -0700

    new related user authorization

--- manticore_tastypie_user/authorization.py
+++ manticore_tastypie_user/authorization.py
@@ -67,3 +67,41 @@ class UserObjectsOnlyAuthorization(Authorization):
 
     def delete_detail(self, object_list, bundle):
         return bundle.obj.user == bundle.request.user
+
+
+class RelateUserAuthorization(Authorization):
+    def read_list(self, object_list, bundle):
+        return object_list
+
+    def read_detail(self, object_list, bundle):
+        return True
+
+    def create_list(self, object_list, bundle):
+        raise Unauthorized("Sorry, no bulk creates.")
+
+    def create_detail(self, object_list, bundle):
+        if not bundle.request.user.is_authenticated():
+            raise Unauthorized("Creating requires an authenticated user.")
+
+        bundle.obj.user = bundle.request.user
+        return True
+
+    def update_list(self, object_list, bundle):
+        allowed = []
+
+        # Since they may not all be saved, iterate over them.
+        for obj in object_list:
+            if obj.user == bundle.request.user:
+                allowed.append(obj)
+
+        return allowed
+
+    def update_detail(self, object_list, bundle):
+        return bundle.obj.user == bundle.request.user
+
+    def delete_list(self, object_list, bundle):
+        # Sorry user, no deletes for you!
+        raise Unauthorized("Sorry, no bulk deletes.")
+
+    def delete_detail(self, object_list, bundle):
+        return bundle.obj.user == bundle.request.user
