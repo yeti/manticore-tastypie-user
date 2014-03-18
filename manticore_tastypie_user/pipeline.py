47af06fc9b455158d7917eddc80e759de0e45411
Rudy Mutter <rudy@yetihq.com>
2014-03-18 12:06:29 -0700

    migrating custom social pipeline to python social auth

--- manticore_tastypie_user/pipeline.py
+++ manticore_tastypie_user/pipeline.py
@@ -8,36 +8,35 @@ from django.core.files import File
 import oauth2
 
 
-def social_auth_user(backend, uid, user=None, *args, **kwargs):
+def social_auth_user(strategy, uid, user=None, *args, **kwargs):
     """Return UserSocialAuth account for backend/uid pair or None if it
     doesn't exists.
 
     Delete UserSocialAuth if UserSocialAuth entry belongs to another
     user.
     """
-    social_user = UserSocialAuth.get_social_auth(backend.name, uid)
-    if social_user:
-        if user and social_user.user != user:
+    social = UserSocialAuth.get_social_auth(strategy.backend.name, uid)
+    if social:
+        if user and social.user != user:
             # Delete UserSocialAuth pairing so this account can now connect
-            social_user.delete()
-            social_user = None
+            social.delete()
+            social = None
         elif not user:
-            user = social_user.user
-    return {'social_user': social_user,
+            user = social.user
+    return {'social': social,
             'user': user,
+            'is_new': user is None,
             'new_association': False}
 
 
-def get_profile_image(backend, details, response, uid, user=None, social_user=None, *args, **kwargs):
+def get_profile_image(strategy, details, response, uid, user, social, *args, **kwargs):
     """Attempt to get a profile image for the User"""
 
     if user is None:
         return
 
-    # TODO: profile picture urls are already included in the 'response' object
-
     # Save photo from FB
-    if backend.name == "facebook":
+    if strategy.backend.name == "facebook":
         try:
             image_url = "https://graph.facebook.com/%s/picture?type=large" % uid
             result = urllib.urlretrieve(image_url)
@@ -57,12 +56,12 @@ def get_profile_image(backend, details, response, uid, user=None, social_user=No
                     done = True
         except URLError:
             pass
-    elif backend.name == "twitter" and social_user:
+    elif strategy.backend.name == "twitter" and social:
         try:
             # Get user info from twitter
             user_url = "http://api.twitter.com/1.1/users/show.json?user_id=%s&include_entities=false" % uid
             consumer = oauth2.Consumer(settings.TWITTER_CONSUMER_KEY, settings.TWITTER_CONSUMER_SECRET)
-            token = oauth2.Token(social_user.tokens['oauth_token'], social_user.tokens['oauth_token_secret'])
+            token = oauth2.Token(social.tokens['oauth_token'], social.tokens['oauth_token_secret'])
             client = oauth2.Client(consumer, token)
             resp, content = client.request(user_url)
             twitter_user = json.loads(content)
